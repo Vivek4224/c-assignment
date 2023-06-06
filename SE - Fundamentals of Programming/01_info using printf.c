c420df053e868fa0407de0dc37f9bf651f754bdc
vivekbarva <131164300+vivekbarva@users.noreply.github.com>
2023-06-06 17:16:55 +0530

    Add files via upload

---
+++ SE - Fundamentals of Programming/01_info using printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+
+{
+	printf("My Name Is Vivek\n");
+	printf("My Birthdate Is 22/08/2003\n");
+	printf("My Age Is 19\n");
+	printf("My Address Is Kamrej\n");
+	
+	return 0;
+}
